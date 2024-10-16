d6a4f232649daa5cf5fd5cfa0b747766a95b1098
matevzklancar <matevz.klancar@allianceblock.io>
2024-10-16 14:30:37 +0200

    Added a new endpoint to cors

--- src/index.ts
+++ src/index.ts
@@ -16,7 +16,7 @@ const app = new Hono();
 app.use(
   '*',
   cors({
-    origin: ['http://localhost:5173'],
+    origin: ['http://localhost:5173', 'https://zvestserver.onrender.com/'],
     allowMethods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowHeaders: ['Content-Type', 'Authorization'],
     exposeHeaders: ['Content-Length'],
