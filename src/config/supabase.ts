1c8a4c7d463ecbeb9143b13016be4a95281d02a4
matevzklancar <matevz1998@gmail.com>
2025-03-30 13:49:18 +0200

    fixed hardcoded env

--- src/config/supabase.ts
+++ src/config/supabase.ts
@@ -1,8 +1,10 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl =
-  process.env.SUPABASE_URL || 'https://wjmldejbfyyuilqsemsr.supabase.co';
-const supabaseKey =
-  process.env.SUPABASE_ANON_KEY ||
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndqbWxkZWpiZnl5dWlscXNlbXNyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjU1NDMyMzcsImV4cCI6MjA0MTExOTIzN30.X1PIenrSvd9UStFnpxLgI_e-Mhgo4GJUmnwN9sVe9sA';
-export const supabase = createClient(supabaseUrl, supabaseKey);
+if (!process.env.SUPABASE_URL) throw new Error('Missing env.SUPABASE_URL');
+if (!process.env.SUPABASE_ANON_KEY)
+  throw new Error('Missing env.SUPABASE_ANON_KEY');
+
+export const supabase = createClient(
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_ANON_KEY
+);
